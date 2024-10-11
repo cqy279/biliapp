8c09a0ca8e319c94f6524ad64aa3ade22b9609d9
cqy <1849453424@qq.com>
2024-10-11 23:34:23 +0800

    基于文件的路由系统、SEO优化

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <NuxtLink to="/">首页</NuxtLink>
+        <NuxtLink to="/video">视频页</NuxtLink>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="success">成功按钮</van-button>
+        <NuxtPage />
+    </div>
+</template>
+<script setup lang="ts">
+useSeoMeta({
+    // 网站标题
+    title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili官方',
+    // 网站描述
+    description:
+        '哔哩哔哩（bilibili.com)是国内知名的视频弹幕网站，这里有及时的动漫新番，活跃的ACG氛围，有创意的Up主。大家可以在这里找到许多欢乐。',
+    // 搜索关键词
+    keywords: 'B站,bilibili,哔哩哔哩,哔哩哔哩动画,动漫,电影,在线动漫,高清电影',
+    // 绕开 B 站图片防盗链
+    referrer: 'no-referrer',
+})
+</script>
+
+<style lang="scss" scoped>
+@import url('~/assets/style/base.scss');
+@import url('~/assets/style/color.scss');
+</style>
---
+++ page/index/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="success">成功按钮</van-button>
+    </div>
+</template>
+
+<script lang="ts"></script>
+
+<style></style>
